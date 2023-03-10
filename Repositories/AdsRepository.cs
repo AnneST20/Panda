93240edcb92a03503a1b1bbde2a549a1bd5a008e
Anna Stetsenko <anna.stetsenko@valtech.com>
2023-03-10 13:15:23 +0200

    controllers fixed

---
+++ Repositories/AdsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Panda.Repositories
+{
+    public class AdsRepository
+    {
+        public static string GetNewId()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
